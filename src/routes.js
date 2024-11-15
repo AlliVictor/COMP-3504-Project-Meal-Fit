d26fc4f12bdf25eb24e18507af0bcd849aab1814
mlabs976 <superman3524@gmail.com>
2024-11-15 14:41:39 -0700

    Testing api endpoint to view all meals from the database.

--- src/routes.js
+++ src/routes.js
@@ -5,3 +5,17 @@ module.exports.register = (app, database) => {
     app.get('/', async (req, res) => {
         res.status(200).send("This is running!").end();
     });
+
+    // API endpoint to fetch data from the meal_view
+    app.get('/api/meals', (req, res) => {
+        const query = `SELECT * FROM project-meal-planner.meal_view`;
+
+        db.query(query, (err, results) => {
+            if (err) {
+                console.error('Error fetching data from meal_view:', err);
+                res.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+            res.json(results);
+        });
+    });
