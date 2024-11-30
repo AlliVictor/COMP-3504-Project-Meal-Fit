723cdf60e7b15cf579ca80fb5d5a4c79cf6fb415
mlabs976 <superman3524@gmail.com>
2024-11-29 19:20:42 -0700

    small changes again in meal.js

--- src/models/meal.js
+++ src/models/meal.js
@@ -10,7 +10,7 @@ const filterMeals = async (db, dietrestrictions /*allergies*/) => {
   const query = `
     SELECT * FROM meals_view
     WHERE NOT FIND_IN_SET(?, restriction_name)
-//    AND NOT FIND_IN_SET(?, allergies)
+//AND NOT FIND_IN_SET(?, allergies)
   `;
   const meals = await db.query(query, [dietrestrictions /*allergies*/]);
   return meals;
@@ -19,8 +19,8 @@ const filterMeals = async (db, dietrestrictions /*allergies*/) => {
 const generateMealPlan = async (db, dietrestrictions /*allergies*/) => {
   const query = `
     SELECT * FROM meals_view
-    WHERE NOT FIND_IN_SET(?, restriction_name) AND
-//    AND NOT FIND_IN_SET(?, allergies)
+    WHERE NOT FIND_IN_SET(?, restriction_name)
+    //AND NOT FIND_IN_SET(?, allergies)
     ORDER BY RAND()
     LIMIT 21; -- 3 meals a day for 7 days
   `;
